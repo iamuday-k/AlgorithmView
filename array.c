924808f6d37edb601533d93f2111bb0baf51b117
iamuday-k <141856209+iamuday-k@users.noreply.github.com>
2024-01-31 15:54:47 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define ARRAY_SIZE 10
+#define DELAY_MICROSECONDS 10000 //1 SECOND
+
+void printfArray(int arr[],int size,int selected1, int selected2){
+    for(int i=0;i<size;i++){
+        if( i==selected1 || i==selected2){
+            printf("\x1b[31m%d \x1b[0m", arr[i]); // to print the elements in red;
+        }
+        else printf("%d ", arr[i]);
+    }
+    printf("\r");// \r is used to take the cursor back to the start of the printf statement and we used it as we want to override
+    fflush(stdout); //fflush is used to display the output make it visible immediately
+}
+
+
+void swap(int* a,int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void delay(int microseconds) {
+    int nanoseconds = microseconds * 1000;
+    for(int i=0; i<nanoseconds; i++);
+}
+
+void bubblesort(int arr[], int size){
+    for(int i=0; i<size-1; i++){
+        for(int j=0;j<size-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+                printfArray(arr, size, j, j+1);
+                delay(DELAY_MICROSECONDS);
+            }
+        }
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    int arr[ARRAY_SIZE];
+    for(int i=0;i<ARRAY_SIZE;i++){
+        arr[i]=rand() % 100; //generate random intergers for the array
+    }
+
+    printf("Original Array: ");
+    printfArray(arr,ARRAY_SIZE, -1, -1);
+    printf("\n");
+
+    printf("\n Sorting Steps: \n");
+    bubblesort(arr,ARRAY_SIZE);
+    printf("\n");
+    
+
+    return 0;
+}
